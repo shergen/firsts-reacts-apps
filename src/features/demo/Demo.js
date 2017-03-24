4859632373c15b9910deb1f9338d0ddd856bf323
root <root@Familigo>
2017-03-24 10:42:19 +0100

    react bootstrap added to react

--- src/features/demo/Demo.js
+++ src/features/demo/Demo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './rsc/logo.svg';
 import './Demo.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+	<Button>Test</Button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
