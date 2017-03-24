182760838faedc3e18fc4d2f33a35511564b7071
root <root@Familigo>
2017-03-24 11:56:56 +0100

    Installing facebook flow

--- src/features/demo/Demo.js
+++ src/features/demo/Demo.js
@@ -1,7 +1,37 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './rsc/logo.svg';
 import './Demo.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+var bool: boolean = true;
+var nb: number = NaN;
+var str: string = "Flow type tests";
+var nul: null = null;
+var undefine: void = undefined;
+var ArrayOfString: Array<string> = ["Flow","type","test"];
+var ArrayOfInt: Array<number> = [12, 10, NaN];
+var obj: { str: string, nb: number } = {str: "test", nb: NaN};
+var map: { [slug: string]: number} = {test: 12, lol: NaN};
+var any:any = "L"+337;
+var func = function (arg: number): string {
+	return "try smth smooth" + arg;
+};
+var test = async function MayTheForceBeWithYou(yourKindbness: string, func: (arg: number)=> string):Promise<string> {
+	var test = await func(12);
+	return "MayTheForceBeWithYou" + test;
+};
+
+type email = string;
+var ownType: email = "esm@ssmproject.com";
+
+type testType<ptr> = ptr;
+type testObj<ptr> = {test: ptr};
+var val: testType<string> = "blabl";
+var objTest: testObj<string> = {test: "azeazea"};
+var maybe: ?string = null
+
+
 
 class App extends Component {
   render() {
